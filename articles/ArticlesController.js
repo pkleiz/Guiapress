3d577f9c7ff700b0752ccd5cf3b3643cbd493523
Pedro Kleiz <pkleiz@hotmail.com>
2020-07-30 00:54:27 -0300

    configurando offset para limitar o numero de posts

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,16 +52,13 @@ router.post("/articles/delete", (req, res) => {
 
 router.get("/admin/articles/edit/:id", (req, res) => {
   var id = req.params.id;
-  var title = req.body.title;
-  var slug = req.body.slug;
-  var category = req.body.category;
-
   Article.findByPk(id)
     .then((article) => {
       if (article != undefined) {
         Category.findAll().then((categories) => {
           res.render("admin/articles/edit", {
             categories: categories,
+            article: article,
           });
         });
       } else {
@@ -71,6 +68,62 @@ router.get("/admin/articles/edit/:id", (req, res) => {
     .catch((err) => {
       res.redirect("/");
     });
+});
+
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.update(
+    { title: title, body: body, categoryId: category, slug: slugify(title) },
+    {
+      where: {
+        id: id, // atualiza o id do banco igual ao que estou passando na página
+      },
+    }
+  )
+    .then(() => {
+      res.redirect("/admin/articles");
+    })
+    .catch((err) => {
+      res.redirect("/admin/articles");
+    });
+});
+
+router.get("/articles/page/:num", (req, res) => {
+  var page = req.params.num;
+  var offset = 0;
+
+  if (isNaN(page) || page == 1) {
+    offset = 0;
+  } else {
+    offset = parseInt(page) * 4;
+  }
+
+  Article.findAndCountAll({
+    limit: 4, //limita o tamanho
+    offset: offset, //a partir de tal número
+  }).then((articles) => {
+    var next;
+    if (offset + 4 >= articles.count) {
+      next = false;
+    } else {
+      next = true;
+    }
+
+    var result = {
+      next: next,
+      articles: articles,
+    };
+    Category.findAll().then((categories) => {
+      res.render("admin/articles/page.ejs", {
+        result: result,
+        categories: categories,
+      });
+    });
+  });
 });
 
 module.exports = router;
